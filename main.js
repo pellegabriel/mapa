b732b8058dc140df51d78d388cc7ce0ffe11499e
gabriel pelle <gabypelle12@gmail.com>
2022-07-25 14:36:47 -0300

    ..

--- main.js
+++ main.js
@@ -1,17 +1,62 @@
 import './style.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Stamen from 'ol/source/Stamen';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import {Icon, Style} from 'ol/style';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import { fromLonLat } from 'ol/proj';
+
+function createStyle(src, img) {
+  return new Style({
+    image: new Icon({
+      anchor: [0.5, 0.96],
+      crossOrigin: 'anonymous',
+      src: src,
+      img: img,
+      imgSize: img ? [img.width, img.height] : undefined,
+    }),
+  });
+}
+
+const iconFeature = new Feature(
+  new Point([0, 0])
+  );
+iconFeature.set(
+  'style', createStyle(
+    'data/icon.png', undefined
+    )
+  );
 
 const map = new Map({
-  target: 'map',
   layers: [
+    
     new TileLayer({
-      source: new OSM()
+      source: new Stamen({
+        layer: 'watercolor'
+      })
+    }),    
+    new TileLayer({
+      source: new Stamen({
+        layer: 'terrain-labels',
+      }),
+    }),
+    new VectorLayer({
+      style: function(feature){
+        return feature.get('style')
+      },
+      source: new VectorSource({
+        features: [iconFeature]
+      }),
     })
   ],
+  target: document.getElementById('map'),
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: fromLonLat([-57.950069225014374, -34.914163405860094]),
+    zoom: 14,
+    minZoom: 9,
+    maxZoom: 20,
   })
 });
+export default Map;
