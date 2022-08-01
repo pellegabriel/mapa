5f63bb595bc548472a000840efea8eaf16f064be
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2022-08-01 12:04:48 -0300

    ..srciomg

--- main.js
+++ main.js
@@ -1,70 +1,66 @@
-import './style.css';
-import {Map, Tile, View} from 'ol';
+import "./style.css";
+import { Map, Tile, View } from "ol";
 // import OSM from 'ol/source/OSM'
-import Stamen from 'ol/source/Stamen';
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import VectorSource from 'ol/source/Vector';
-import {Icon, Style} from 'ol/style';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import { fromLonLat } from 'ol/proj';
-
-
+import Stamen from "ol/source/Stamen";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import VectorSource from "ol/source/Vector";
+import { Icon, Style } from "ol/style";
+import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
+import { fromLonLat } from "ol/proj";
 
 const iconFeature = new Feature({
-  geometry: new Point(
-    fromLonLat([-57.950069225014374, -34.914163405860094])
-    ),
-    name:'Barril',
-    population: 4000,
-    rainfall: 500
+  geometry: new Point(fromLonLat([-57.950069225014374, -34.914163405860094])),
+  name: "Barril",
+  population: 4000,
+  rainfall: 500,
 });
 const iconStyle = new Style({
   image: new Icon({
-      anchor: [0.5, 0.96],
-      anchorXUnits: 'fraction',
-      anchorYUnits: 'pixels',
-      src: './barril.png'
-    }),
-  });
+    anchor: [0.5, 0.96],
+    anchorXUnits: "pixels",
+    anchorYUnits: "pixels",
+    src: "./barril.png",
+    srcimgSize: [200, 200],
+  }),
+});
 
 iconFeature.setStyle(iconStyle);
 
 const vectorSource = new VectorSource({
-  features: [iconFeature]
+  features: [iconFeature],
 });
 //nose si tiene que ser new Vector ({})
 const vectorLayer = new VectorLayer({
-  source: vectorSource
+  source: vectorSource,
 });
-
 
 const map = new Map({
   layers: [
     // new Tile({
     //   source:new OSM()
-    // }),    
+    // }),
     new TileLayer({
       source: new Stamen({
-        layer: 'toner'
+        layer: "toner",
       }),
     }),
     new VectorLayer({
-      style: function(feature){
-        return feature.get('style')
+      style: function (feature) {
+        return feature.get("style");
       },
       source: new VectorSource({
-        features: [iconFeature]
+        features: [iconFeature],
       }),
     }),
-    vectorLayer
+    vectorLayer,
   ],
-  target: document.getElementById('map'),
+  target: document.getElementById("map"),
   view: new View({
     center: fromLonLat([-57.950069225014374, -34.914163405860094]),
     zoom: 14,
     minZoom: 9,
     maxZoom: 20,
-  })
+  }),
 });
 export default Map;
